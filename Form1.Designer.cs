75982eef801084cb121105f1bfbd6b1e42deb4bb
ppsh7089@gmail.com <ppsh7089@gmail.com>
2020-05-31 17:11:25 +0900

    초기 커밋.

---
+++ Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace web_word
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.sreach_button = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.listView4 = new System.Windows.Forms.ListView();
+            this.listView3 = new System.Windows.Forms.ListView();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.tabPage2.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // sreach_button
+            // 
+            this.sreach_button.BackColor = System.Drawing.Color.Transparent;
+            this.sreach_button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.sreach_button.Font = new System.Drawing.Font("굴림", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.sreach_button.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.sreach_button.Location = new System.Drawing.Point(751, 12);
+            this.sreach_button.Name = "sreach_button";
+            this.sreach_button.Size = new System.Drawing.Size(135, 21);
+            this.sreach_button.TabIndex = 0;
+            this.sreach_button.Text = "검색";
+            this.sreach_button.UseVisualStyleBackColor = false;
+            this.sreach_button.Click += new System.EventHandler(this.Sreach_button_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(9, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(736, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(9, 468);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(877, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.radioButton4);
+            this.tabPage2.Controls.Add(this.radioButton3);
+            this.tabPage2.Controls.Add(this.listView4);
+            this.tabPage2.Controls.Add(this.listView3);
+            this.tabPage2.Controls.Add(this.radioButton2);
+            this.tabPage2.Controls.Add(this.radioButton1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(869, 397);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "뉴스";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 72);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(34, 16);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.Text = "IT";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 50);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(47, 16);
+            this.radioButton3.TabIndex = 4;
+            this.radioButton3.Text = "문화";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // listView4
+            // 
+            this.listView4.HideSelection = false;
+            this.listView4.Location = new System.Drawing.Point(178, 0);
+            this.listView4.Name = "listView4";
+            this.listView4.Size = new System.Drawing.Size(691, 397);
+            this.listView4.TabIndex = 3;
+            this.listView4.UseCompatibleStateImageBehavior = false;
+            this.listView4.SelectedIndexChanged += new System.EventHandler(this.listView4_SelectedIndexChanged);
+            // 
+            // listView3
+            // 
+            this.listView3.HideSelection = false;
+            this.listView3.Location = new System.Drawing.Point(59, 0);
+            this.listView3.Name = "listView3";
+            this.listView3.Size = new System.Drawing.Size(113, 397);
+            this.listView3.TabIndex = 2;
+            this.listView3.UseCompatibleStateImageBehavior = false;
+            this.listView3.SelectedIndexChanged += new System.EventHandler(this.listView3_SelectedIndexChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 6);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(47, 16);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "정치";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 28);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(47, 16);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "경제";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(9, 39);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(877, 423);
+            this.tabControl1.TabIndex = 6;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.listView1);
+            this.tabPage1.Controls.Add(this.listView2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(869, 397);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "클라이언트";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(126, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(743, 397);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // listView2
+            // 
+            this.listView2.HideSelection = false;
+            this.listView2.Location = new System.Drawing.Point(0, 0);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(120, 397);
+            this.listView2.TabIndex = 5;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.SelectedIndexChanged += new System.EventHandler(this.listView2_SelectedIndexChanged);
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.webBrowser1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(869, 397);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "둠";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            this.tabPage3.Click += new System.EventHandler(this.tabPage3_Click);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(3, 3);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(863, 391);
+            this.webBrowser1.TabIndex = 0;
+            this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::web_word.Properties.Resources._800px_Flag_of_the_new_USSR__2__svg;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(893, 499);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.sreach_button);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "검색";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button sreach_button;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.ListView listView4;
+        private System.Windows.Forms.ListView listView3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+using System.Net.Sockets;
+
+
+namespace web_word
+{
+    public partial class Form1 : Form
+    {
+        TcpClient tc;
+        NetworkStream stream;
+        public Form1()
+        {
+            InitializeComponent();
+            // 디폴트값 사용 (Maximum=100, Minimum=0, Step=10)
+            progressBar1.Style = ProgressBarStyle.Blocks;
+            //tc = new TcpClient("127.0.0.1", 9999);
+           // stream = tc.GetStream();
+            
+            
+        }
+        private void Sreach_button_Click(object sender, EventArgs e)
+        {
+            
+            listView1.HotTracking = true;
+            if (textBox1.Text.Equals(""))
+            {
+                MessageBox.Show("검색창을 작성하세요!");
+            }
+            else
+            {
+                String[] texts = new String[1];
+                String[] arr = new String[3];
+                string query = textBox1.Text; // 검색할 문자열
+                string url = "https://openapi.naver.com/v1/search/blog?query=" + query; // 결과가 JSON 포맷
+                                                                                        // string url = "https://openapi.naver.com/v1/search/blog.xml?query=" + query;  // 결과가 XML 포맷
+               
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                    request.Headers.Add("X-Naver-Client-Id", "epyY5bXZb2npJsGQ3p1u"); // 클라이언트 아이디
+                    request.Headers.Add("X-Naver-Client-Secret", "Lzwj3w_ZWo");       // 클라이언트 시크릿
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                    string status = response.StatusCode.ToString();
+              
+               
+                
+                if (status == "OK")
+                {
+                    Stream steam = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(steam, Encoding.UTF8);
+                    string text = reader.ReadToEnd();
+                    Console.WriteLine(text);
+                    JObject o = JObject.Parse(text);
+                    JArray ant = (JArray)o["items"];
+                    progressBar1.Value = 0;
+                    String[] kok = new string[1];
+                    kok[0] = text;
+                    ListViewItem it2 = new ListViewItem(kok);
+                    listView2.Items.Add(textBox1.Text);
+                    
+                    //byte[] buff = Encoding.UTF8.GetBytes(text.ToString());
+                    //stream.Write(buff,0,buff.Length);
+                    for (int i = 0; i < ant.Count; i++)
+                    {
+                        tc = new TcpClient("localhost", 7000);
+                        stream = tc.GetStream();
+
+                        var json = new JObject();
+                        JObject j = (JObject)ant[i];
+                        string title = (string)j["title"];
+                        title = Regex.Replace(title, "(<([^>]+)>)", "");
+                        string description = (string)j["description"];
+                        description = Regex.Replace(description, "(<([^>]+)>)", "");
+                        string link = (string)j["link"];
+                        link = Regex.Replace(link, "(<([^>]+)>)", "");
+                        link = "<a href=" + link + "link</a>";
+                        json.Add("title", title);
+                        json.Add("description", description);
+
+                        byte[] buff1 = Encoding.UTF8.GetBytes(json.ToString());
+              
+                        //검색어 집어 넣기
+                        arr[0] = title;
+                        arr[1] = description;
+                        arr[2] = link;
+                        ListViewItem it = new ListViewItem(arr);
+                        listView1.Items.Add(it);
+                        progressBar1.PerformStep();
+                        string str1 = Encoding.UTF8.GetString(buff1);                      
+                        
+
+                        stream.Write(buff1, 0, buff1.Length);
+                        //Console.WriteLine("송신: {0}", "buff");
+                        stream.Close();
+                        
+                    }
+                    tc.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Error 발생=" + status);
+                }   
+            }
+        }
+        
+
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listView1.View = View.Details;
+            listView1.Columns.Add("제목", 246);
+            listView1.Columns.Add("내용", 246);
+            listView1.Columns.Add("주소", 246);
+            listView1.GridLines = true;
+            listView1.FullRowSelect = true;
+            listView2.CheckBoxes = true;
+            listView2.View = View.Details;
+            listView2.Columns.Add("검색기록", 116);
+            listView2.GridLines = true;
+            listView2.FullRowSelect = true;
+            listView3.View = View.Details;
+            listView3.Columns.Add("검색기록", 116);
+            listView3.GridLines = true;
+            listView3.FullRowSelect = true;
+            listView4.View = View.Details;
+            listView4.Columns.Add("제목", 350);
+            listView4.Columns.Add("내용", 350);
+            //listView4.Columns.Add("", );
+            listView4.GridLines = true;
+            listView4.FullRowSelect = true;
+
+        }
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView4_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+
+        }
+    }
+}
+
